48c7829da3f321b5a92568b748b72c308d62fbb2
atefe fazeli <atefefazeli1996@gmail.com>
2022-05-27 14:39:13 +0430

    app start

---
+++ src/components/Portfolio.js
@@ -0,0 +1,20 @@
+import Port from '../images/port.jpg';
+import React from 'react';
+import styles from './Portfolio.module.css';
+const Portfolio = () => {
+    return (
+
+        <div className={styles.portfoliocontainer}>
+            {/* <img className={styles.portfolioimg} src={Port} alt=" image" />
+                    <div className={styles.portfolioParacontainer}  >
+                        Photography Project
+                        Lorem ipsem Lorem ipsemLorem
+                         Lorem ipsemLorem ipsemLorem
+                         Lorem ipsemLorem ipsemLorem
+                    </div>
+            <button>View Project</button> */}
+        </div>
+    );
+};
+
+export default Portfolio;
---
+++ src/components/TopPortfolioContainer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import HorizontalScroll from 'react-scroll-horizontal';
+import './TopPortfolio.css';
+const TopPortfolioContainer = () => {
+    return (
+        <div>
+          <HorizontalScroll>
+            <div>
+                        Photography Project
+                        Lorem ipsem Lorem ipsemLorem
+                         Lorem ipsemLorem ipsemLorem
+                         Lorem ipsemLorem ipsemLorem
+             </div>
+            <button>View Project</button>
+          </HorizontalScroll>
+        </div>
+    );
+};
+
+export default TopPortfolioContainer;
